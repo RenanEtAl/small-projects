dfae38e040e3cac405d9bcb10bf38e71afbcf41c
Renanie Tanola <enan.tanola@gmail.com>
2020-07-07 21:26:02 -0700

    header, display, main components, styles

---
+++ password-generator/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Main from "./components/Main";
+
+const App = () => {
+  return (
+    <div className="container">
+      <Main />
+    </div>
+  );
+};
+
+export default App;
