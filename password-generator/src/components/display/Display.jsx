4d2a56345e32e2984a10ba7c318ef4edf98e404b
Renanie Tanola <enan.tanola@gmail.com>
2020-07-09 23:37:26 -0700

    display and header components html

--- password-generator/src/components/display/Display.jsx
+++ password-generator/src/components/display/Display.jsx
@@ -1,11 +1,25 @@
-import React from 'react'
-
+import React from "react";
+import "./Display.css";
 const Display = () => {
-    return (
-        <div>
-            
-        </div>
-    )
-}
+  return (
+    <>
+      <div className="row">
+        <div className='"col-12 password-display-container"'>
+          <div>
+            <div className="password-display">
+              <input className="password-display-input" type="text"></input>
+            </div>
 
-export default Display
+            <div className="password-description">
+              <i className="fas fa-check-circle"> Strong Password</i>
+            </div>
+
+            <div className="password-display-icons"></div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Display;
--- password-generator/src/components/header/Header.jsx
+++ password-generator/src/components/header/Header.jsx
@@ -1,11 +1,16 @@
-import React from 'react'
-
+import React from "react";
+import "./Header.css";
 const Header = () => {
-    return (
-        <div>
-            
+  return (
+    <div className="row">
+      <div className="col-md-12 header">
+        <h1 className="h1">Strong Password Generator</h1>
+        <div className="col-md-12">
+          <h4> Create strong passwords</h4>
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
-export default Header
+export default Header;
